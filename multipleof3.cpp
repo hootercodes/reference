d5d01acfce15c03822a0f3a80571eae7271edc0f
KKKhan Yosef <91080451+emptyIIknowledge@users.noreply.github.com>
2021-09-20 14:33:23 -0400

    Update multipleof3.cpp

--- multipleof3.cpp
+++ multipleof3.cpp
@@ -1,9 +1,3 @@
-/* Given an array of integers, return an array of multiple-of-3-indexed elements in the original array, in reversed order.
-
-For example, suppose the original array is [2, 1, 3, 6, 5]. Then elements whose indexes are a multiple of 3 are 2 and 6. The returned array is [6, 2].
-
-Hint: you may use stack.*/
-
 #include<bits/stdc++.h> 
 using namespace std ;
 int  solve(int n, int a[])
